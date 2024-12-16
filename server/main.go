a0551f61a55b18f9dc4e057a91d86faba77cf753
Anideep-Kalia <anideepkalia@gmail.com>
2024-12-17 02:00:57 +0530

    well  welll well it's a tough machine

--- server/main.go
+++ server/main.go
@@ -21,4 +21,13 @@ func main(){
 	router.Use(cors.Default())
 
 	router.POST("/entry/create", routes.AddEntry)
+	router.GET("entries",routes.GetEnteries)
+	router.GET("entry/:id/",routes.GetEntryById)
+	router.GET("/ingredient/:ingredient", routes.GetEnteriesByIngredient)
+
+	router.PUT("/entry/update/:id",routes.UpdateEntry)
+	router.PUT("/ingrdient/update/:id", routes.UpdateIngredient)
+	router.DELETE("/entry/delete/:id", routes.DeleteEntry)
+	router.Run(":" + port)
+
 }
