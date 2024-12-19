ed63326582aaedbde82eb2d9c31c993b6a068c47
Anideep-Kalia <anideepkalia@gmail.com>
2024-12-20 01:26:25 +0530

    added some comments and some understanding of the AddEnrty function

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -9,19 +9,21 @@ import (
 	"github.com/Anideep-Kalia/GO-React-Mongo-Crud/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// Package level variables initialised when packages are imported
 var validate = validator.New()
 var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
 
-
+// adding enteries into DB
 func AddEntry(c *gin.Context) {
-	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-	var entry models.Entry
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)  	//context is package used to manage API calls like deadline, timeout etc..
+	var entry models.Entry   														// from models folder
 
-	if err := c.BindJSON(&entry); err != nil {
+	if err := c.BindJSON(&entry); err != nil {										// incoming json body conversion to struct (not a ternary operation)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		fmt.Println(err)
 		return
@@ -43,3 +45,4 @@ func AddEntry(c *gin.Context) {
 	defer cancel()
 	c.JSON(http.StatusOK, result)
 }
+
