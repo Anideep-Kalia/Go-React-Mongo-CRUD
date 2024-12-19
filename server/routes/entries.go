e158c8866e0f01180b4e4e5527c1647ff73cb5ce
Anideep-Kalia <anideepkalia@gmail.com>
2024-12-20 04:19:48 +0530

    UpdateIngredient function completed

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -23,12 +23,13 @@ func AddEntry(c *gin.Context) {				// c.JSON -> just like a response
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)  	//context is package used to manage API calls like deadline, timeout etc..
 	var entry models.Entry   														// from models folder
 
-	if err := c.BindJSON(&entry); err != nil {										// incoming json body conversion to struct (not a ternary operation)
+	// checks if incoming json body is in format of models.Entry and types of fields i.e. having same fields but it doesn't match limitations on data type like Fat<=100%
+	if err := c.BindJSON(&entry); err != nil {										
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		fmt.Println(err)
 		return
 	}
-	validationErr := validate.Struct(entry)
+	validationErr := validate.Struct(entry)		//this checks the limitations of the fields and checks for required fields
 	if validationErr != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": validationErr.Error()})	//gin.H is used to mapping so message can be sent in tabular format
 		fmt.Println(validationErr)
@@ -71,7 +72,7 @@ func GetEntries(c *gin.Context) {
 
 // Literally same function as above just with tweaks to get some specific results
 func GetEntriesByIngredient(c *gin.Context) {
-	ingredient := c.Params.ByName("id")
+	ingredient := c.Params.ByName("id")												// getting id from URL params, will be getting in hexadecimal
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 	var entries []bson.M
 	cursor, err := entryCollection.Find(ctx, bson.M{"ingredients": ingredient})		//ingredients are inserted to get specific results
@@ -94,7 +95,7 @@ func GetEntriesByIngredient(c *gin.Context) {
 // Again same fucntion but now for specific IDs
 func GetEntryById(c *gin.Context) {
 	EntryID := c.Params.ByName("id")
-	docID, _ := primitive.ObjectIDFromHex(EntryID)
+	docID, _ := primitive.ObjectIDFromHex(EntryID)									// Converting hexadecimal id from params into objectId suitable for mongo
 
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 	var entry bson.M
@@ -108,3 +109,30 @@ func GetEntryById(c *gin.Context) {
 	c.JSON(http.StatusOK, entry)
 }
 
+func UpdateIngredient(c *gin.Context) {
+	entryID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(entryID)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	type Ingredient struct {							// made a new struct so just to match incoming json fields and data type
+		Ingredients *string `json:"ingredients"`
+	}
+	var ingredient Ingredient
+
+	if err := c.BindJSON(&ingredient); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	result, err := entryCollection.UpdateOne(ctx, bson.M{"_id": docID},
+		bson.D{{"$set", bson.D{{"ingredients", ingredient.Ingredients}}}},
+	)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result.ModifiedCount)
+}
